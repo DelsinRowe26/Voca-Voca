7c4eaee1a7d80b654bfffea579240aff7f16d9f0
DelsinRowe26 <ilya.gritchin20020909@gmail.com>
2022-09-21 22:11:23 +0700

    Добавьте файлы проекта.

---
+++ Voca-Voca/MainWindow.xaml.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CSCore;
+using CSCore.Codecs;
+using CSCore.CoreAudioAPI;
+using CSCore.SoundIn;
+using CSCore.SoundOut;
+using CSCore.Streams;
+
+namespace Voca_Voca
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private FileInfo fileInfo1 = new FileInfo("Data_Load.tmp");
+
+        private SimpleMixer mMixer, mMixerRight;
+        private int SampleRate;//44100;
+        //private Equalizer equalizer;
+        private WasapiOut mSoundOut, mSoundOut1;
+        private WasapiCapture mSoundIn, mSoundIn1;
+        private SampleDSPPitch mDspRec;
+        private SampleDSPTurbo mDspTurbo, mDspTurbo1;
+        private IWaveSource mSource;
+        private ISampleSource mMp3;
+        private MMDeviceCollection mOutputDevices;
+        private MMDeviceCollection mInputDevices;
+
+        string langindex;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Voca_Voca_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (SoftCl.IsSoftwareInstalled("Microsoft Visual C++ 2015-2022 Redistributable (x86) - 14.32.31332") == false)
+                {
+                    Process.Start("VC_redist.x86.exe");
+                }
+
+
+
+                //Находит устройства для захвата звука и заполнияет комбобокс
+                MMDeviceEnumerator deviceEnum = new MMDeviceEnumerator();
+                mInputDevices = deviceEnum.EnumAudioEndpoints(DataFlow.Capture, DeviceState.Active);
+                MMDevice activeDevice = deviceEnum.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Multimedia);
+
+                SampleRate = activeDevice.DeviceFormat.SampleRate;
+
+                foreach (MMDevice device in mInputDevices)
+                {
+                    cmbInput.Items.Add(device.FriendlyName);
+                    if (device.DeviceID == activeDevice.DeviceID) cmbInput.SelectedIndex = cmbInput.Items.Count - 1;
+                }
+
+
+                //Находит устройства для вывода звука и заполняет комбобокс
+                activeDevice = deviceEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+                mOutputDevices = deviceEnum.EnumAudioEndpoints(DataFlow.Render, DeviceState.Active);
+
+                foreach (MMDevice device in mOutputDevices)
+                {
+                    cmbOutput.Items.Add(device.FriendlyName);
+                    if (device.DeviceID == activeDevice.DeviceID) cmbOutput.SelectedIndex = cmbOutput.Items.Count - 1;
+                }
+
+                string[] filename = File.ReadAllLines(fileInfo1.FullName);
+                if (filename.Length == 1)
+                {
+                    Languages();
+                }
+                if (!File.Exists("log.tmp"))
+                {
+                    File.Create("log.tmp").Close();
+                }
+                else
+                {
+                    if (File.ReadAllLines("log.tmp").Length > 1000)
+                    {
+                        File.WriteAllText("log.tmp", " ");
+                    }
+                }
+
+                if (langindex == "0")
+                {
+                    string msg = "Подключите проводную аудио-гарнитуру к компьютеру.\nЕсли на данный момент гарнитура не подключена,\nто подключите проводную гарнитуру, и перезапустите программу для того, чтобы звук подавался в наушники.";
+                    MessageBox.Show(msg);
+                }
+                else
+                {
+                    string msg = "Connect a wired audio headset to your computer.\nIf a headset is not currently connected,\nthen connect a wired headset and restart the program so that the sound is played through the headphones.";
+                    MessageBox.Show(msg);
+                }
+
+                TembroClass tembro = new TembroClass();
+                tembro.Tembro(SampleRate, "Wide_voice_effect.tmp");
+            }
+            catch (Exception ex)
+            {
+                if (langindex == "0")
+                {
+                    string msg = "Ошибка в Loaded: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+                else
+                {
+                    string msg = "Error in Loaded: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+            }
+        }
+
+        private void Mixer()
+        {
+            try
+            {
+
+                mMixer = new SimpleMixer(1, SampleRate) //стерео, 44,1 КГц
+                {
+                    //Right = true,
+                    //Left = true,
+                    FillWithZeros = true,
+                    DivideResult = true, //Для этого установлено значение true, чтобы избежать звуков тиков из-за превышения -1 и 1.
+                };
+            }
+            catch (Exception ex)
+            {
+                if (langindex == "0")
+                {
+                    string msg = "Ошибка в Mixer: \r\n" + ex.Message;
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+                else
+                {
+                    string msg = "Error in Mixer: \r\n" + ex.Message;
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+            }
+        }
+
+        private void Stop()
+        {
+            try
+            {
+                if (mMixer != null)
+                {
+                    mMixer.Dispose();
+                    mMp3.ToWaveSource(32).Loop().ToSampleSource().Dispose();
+                    mMixer = null;
+                }
+                if (mSoundOut != null)
+                {
+                    mSoundOut.Stop();
+                    mSoundOut.Dispose();
+                    mSoundOut = null;
+                }
+                if (mSoundIn != null)
+                {
+                    mSoundIn.Stop();
+                    mSoundIn.Dispose();
+                    mSoundIn = null;
+                }
+                if (mSource != null)
+                {
+                    mSource.Dispose();
+                    mSource = null;
+                }
+                if (mMp3 != null)
+                {
+                    mMp3.Dispose();
+                    mMp3 = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                /*if (langindex == "0")
+                {
+                    string msg = "Ошибка в Stop: \r\n" + ex.Message;
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+                else
+                {
+                    string msg = "Error in Stop: \r\n" + ex.Message;
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }*/
+            }
+        }
+
+        private void SoundIn()
+        {
+            mSoundIn = new WasapiCapture(/*false, AudioClientShareMode.Exclusive, 1*/);
+            Dispatcher.Invoke(() => mSoundIn.Device = mInputDevices[cmbInput.SelectedIndex]);
+            mSoundIn.Initialize();
+            mSoundIn.Start();
+        }
+
+        private void Voca_Voca_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Stop();
+            Environment.Exit(0);
+        }
+
+        private ChannelMask SoundOut()
+        {
+            try
+            {
+
+                mSoundOut = new WasapiOut(/*false, AudioClientShareMode.Exclusive, 1*/);
+                Dispatcher.Invoke(() => mSoundOut.Device = mOutputDevices[cmbOutput.SelectedIndex]);
+                //mSoundOut.Device = mOutputDevices[cmbOutput.SelectedIndex];
+
+
+
+                mSoundOut.Initialize(mMixer.ToWaveSource(32).ToMono());
+
+
+                mSoundOut.Play();
+                mSoundOut.Volume = 10;
+                return ChannelMask.SpeakerFrontLeft;
+            }
+            catch (Exception ex)
+            {
+                if (langindex == "0")
+                {
+                    string msg = "Ошибка в SoundOut: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                    return ChannelMask.SpeakerFrontLeft;
+                }
+                else
+                {
+                    string msg = "Error in SoundOut: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                    return ChannelMask.SpeakerFrontLeft;
+                }
+            }
+        }
+
+        private async void StartFullDuplex()//запуск пича и громкости
+        {
+            try
+            {
+                //Запускает устройство захвата звука с задержкой 1 мс.
+                //await Task.Run(() => SoundIn());
+                SoundIn();
+
+                var source = new SoundInSource(mSoundIn) { FillWithZeros = true };
+
+                //Init DSP для смещения высоты тона
+                mDspTurbo = new SampleDSPTurbo(source.ToSampleSource()/*.AppendSource(Equalizer.Create10BandEqualizer, out mEqualizer)*/.ToMono());
+
+                //SetPitchShiftValue();
+
+                //Инициальный микшер
+                Mixer();
+
+                //Добавляем наш источник звука в микшер
+                mMixer.AddSource(mDspTurbo.ChangeSampleRate(mMixer.WaveFormat.SampleRate));
+
+                //Запускает устройство воспроизведения звука с задержкой 1 мс.
+                await Task.Run(() => SoundOut());
+
+            }
+            catch (Exception ex)
+            {
+                if (langindex == "0")
+                {
+                    string msg = "Ошибка в StartFullDuplex: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+                else
+                {
+                    string msg = "Error in StartFullDuplex: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+            }
+            //return false;
+        }
+
+        private async void Sound(string file)
+        {
+            try
+            {
+                    Mixer();
+                    mMp3 = CodecFactory.Instance.GetCodec(@"Voca-Voca\record\Muisc.wav").ToMono().ToSampleSource();
+                    mDspRec = new SampleDSPPitch(mMp3.ToWaveSource(32).ToSampleSource());
+                    //SampleRate = mDspRec.WaveFormat.SampleRate;
+                    mMixer.AddSource(mDspRec.ChangeSampleRate(mDspRec.WaveFormat.SampleRate).ToWaveSource(32).Loop().ToSampleSource());
+                    await Task.Run(() => SoundOut());
+            }
+            catch (Exception ex)
+            {
+                if (langindex == "0")
+                {
+                    string msg = "Ошибка в Sound: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+                else
+                {
+                    string msg = "Error in Sound: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+            }
+        }
+
+        private void Languages()
+        {
+            try
+            {
+                StreamReader FileLanguage = new StreamReader("Data_Language.tmp");
+                File.WriteAllText("Data_Load.tmp", "1");
+                File.WriteAllText("DataTemp.tmp", "0");
+                langindex = FileLanguage.ReadToEnd();
+                if (langindex == "0")
+                {
+                    lbSetMicr.Content = "Выбор микрофона";
+                    lbSetSpeak.Content = "Выбор динамиков";
+                }
+                else if (langindex != "0")
+                {
+                    lbSetMicr.Content = "Microphone selection";
+                    lbSetSpeak.Content = "Speaker selection";
+                }
+            }
+            catch (Exception ex)
+            {
+                if (langindex == "0")
+                {
+                    string msg = "Ошибка в Languages: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+                else
+                {
+                    string msg = "Error in Languages: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+            }
+        }
+    }
+}
---
+++ Voca-Voca/SampleDSPRecord.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using CSCore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SimpleNeurotuner
+{
+    class SampleDSPRecord : ISampleSource
+    {
+        ISampleSource mSource;
+        //public float[] freq;
+        public SampleDSPRecord(ISampleSource source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            mSource = source;
+            PitchShift = 1;
+        }
+        public /*async Task<int>*/ int Read(float[] buffer, int offset, int count)
+        {
+            try
+            {
+                //double[] buffer1 = new double[count];
+                //double closestfreq = 0;
+                float gainAmplification = (float)(Math.Pow(10.0, (GainDB) / 20.0));//получить Усиление
+                int samples = mSource.Read(buffer, offset, count);//образцы
+                float AmpSrR = 0, AmpSr, Kamp, dKamp, stK;                                                  //if (gainAmplification != 1.0f) 
+                                                                                                            //{
+                for (int i = offset; i < offset + samples; i++)
+                {
+                    //buffer[i] = Math.Max(Math.Min(buffer[i] * gainAmplification, 1), -1);
+                    AmpSrR += Math.Abs(buffer[i]);
+                }
+
+                PitchShifter.AmpDSPR += AmpSrR;
+                PitchShifter.ItDSPR += samples;
+
+                if (PitchShifter.ItDSP >= 5000)
+                {
+                    AmpSr = PitchShifter.AmpDSP / PitchShifter.ItDSP;
+                    AmpSrR = PitchShifter.AmpDSPR / PitchShifter.ItDSPR;
+                    PitchShifter.AmpDSP = 0;
+                    PitchShifter.AmpDSPR = 0;
+                    PitchShifter.ItDSP = 0;
+                    PitchShifter.ItDSPR = 0;
+                    PitchShifter.Kampp = PitchShifter.Kamp;
+                    if (Math.Abs(AmpSrR) < 0.001f)
+                        Kamp = 0;
+                    else
+                        Kamp = AmpSr / AmpSrR;
+                    if (Kamp < 0.05)
+                        Kamp = 0;
+                    Kamp = (float)((int)((Kamp + 0.05) * 10)) / 10;
+                    PitchShifter.Kamp = Kamp;
+                }
+                if (PitchShifter.Kamp < 0)
+                {
+                    Kamp = 0;
+                    dKamp = 0;
+                }
+                else
+                {
+                    dKamp = PitchShifter.Kamp - PitchShifter.Kampp;
+                    Kamp = PitchShifter.Kampp;
+                }
+                for (int i = offset; i < offset + samples; i++)
+                {
+                    Kamp += dKamp / (float)(samples);
+                    buffer[i] = Math.Max(Math.Min(buffer[i] * Kamp, 1), -1);
+                    //if (Math.Abs(buffer[i]) < 0.001f)
+                    //    buffer[i] = 0;
+                }
+                PitchShifter.Kampp = PitchShifter.Kamp;
+                ///<summary>
+                ///int len = buffer.Length;
+                ///freq = buffer;
+                ///await Task.Run(() => FrequencyUtils.FindFundamentalFrequency(buffer, mSource.WaveFormat.SampleRate, 31, 16000));
+                ///FrequencyUtilsRec.FindFundamentalFrequency(buffer, mSource.WaveFormat.SampleRate, 30, mSource.WaveFormat.SampleRate / 2);
+                ///freq = FrequencyUtils.FindFundamentalFrequency(buffer, mSource.WaveFormat.SampleRate, 31, 16000);
+                ///await Task.Run(() => PitchShifter.FindClosestNote(FrequencyUtils.FindFundamentalFrequency(buffer, mSource.WaveFormat.SampleRate, 31, 16000), out closestfreq));
+                ///PitchShifter.FindClosestNote(FrequencyUtilsRec.FindFundamentalFrequency(buffer, mSource.WaveFormat.SampleRate, 30, mSource.WaveFormat.SampleRate / 2), out closestfreq);
+                ///await Task.Run(() => File.WriteAllText("ClosestFreq.txt", closestfreq.ToString()));
+                ///File.WriteAllText("FreqClosestRec.txt", closestfreq.ToString());
+                ///await Task.Run(() => File.AppendAllText("Freq.txt", FrequencyUtils.FindFundamentalFrequency(buffer, mSource.WaveFormat.SampleRate, 31, 16000).ToString("f3") + "\n"));
+                ///File.AppendAllText("FreqRecord.txt", FrequencyUtilsRec.FindFundamentalFrequency(buffer, mSource.WaveFormat.SampleRate, 30, mSource.WaveFormat.SampleRate / 2).ToString("f3") + "\n");
+                ///}
+                ///</summary>
+
+                /*PitchShifter.PitchShift(PitchShift, offset, count, 4096, 4, mSource.WaveFormat.SampleRate, buffer);
+
+                if (PitchShift != 1.0f)
+                {
+                    //FrequencyUtils.FindFundamentalFrequency(buffer1, mSource.WaveFormat.SampleRate, 60, 22050);
+                    PitchShifter.PitchShift(PitchShift, offset, count, 4096, 4, mSource.WaveFormat.SampleRate, buffer);
+
+                }*/
+
+                return samples;
+            }
+            catch
+            {
+                return 0; 
+            }
+        }
+
+        public float GainDB { get; set; }
+
+        public float PitchShift { get; set; }
+
+        public bool CanSeek
+        {
+            get { return mSource.CanSeek; }
+        }
+
+        public WaveFormat WaveFormat
+        {
+            get { return mSource.WaveFormat; }
+        }
+
+        public long Position
+        {
+            get
+            {
+                return mSource.Position;
+            }
+            set
+            {
+                mSource.Position = value;
+            }
+        }
+
+        public long Length
+        {
+            get { return mSource.Length; }
+        }
+
+        public void Dispose()
+        {
+            if (mSource != null) mSource.Dispose();
+        }
+    }
+}
